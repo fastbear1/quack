fa1df5180d21d867c4674f4f008706cfbfb7da54
Andrey Sorokin <andreysrkn@gmail.com>
2026-06-04 18:27:59 +0300

    parser for execute commands

--- cmd/main.go
+++ cmd/main.go
@@ -5,7 +5,8 @@ import (
 	"fmt"
 )
 
-const todoList = `TODO:
+const (
+	todoList = `TODO:
 - parse config file
 - parse command line args
 - parse command line flags
@@ -23,6 +24,19 @@ const todoList = `TODO:
 - interface for all tables, column , index, constraints 
 - independent drivers`
 
+	helpInfo = `Quack - generate migration file for goose according gorm struct models 
+information and database state. Use config file quack_config.yaml for running params.
+Uasge:
+	quack command [flags]
+Commands:
+	- run - quack(run and create) goose migration file
+	- help - show help information
+	- version - show current version(can be used for checking config file)
+flags:
+	- 
+`
+)
+
 func main() {
 	//todo := flag.String("h", "", "Print todo list")
 	numbPtr := flag.Int("numb", 0, "int number")
@@ -32,15 +46,23 @@ func main() {
 	} else {
 		fmt.Println(*numbPtr)
 	}
-	fmt.Println(flag.Args())
-	fmt.Println("tail:", flag.Args())
+	var commands []string = flag.Args()
+	fmt.Println(commands)
+	fmt.Println("tail:", commands)
 
-	for _, w := range flag.Args() {
-		switch w {
+	if len(commands) > 0 {
+		switch commands[0] {
 		case "help":
-			fmt.Println("Print help")
+			fmt.Println(helpInfo)
 		case "todo":
 			fmt.Println(todoList)
+		case "run":
+			fmt.Println("Quacking migration file")
+		default:
+			fmt.Println("Any command presented, use help to view usefull information")
 		}
+	} else {
+		fmt.Println("Any command presented, use help to view usefull information")
 	}
+
 }

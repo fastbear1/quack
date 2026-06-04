96fc9e9dbf22e60c0927f5df4c8f565cab21675e
Andrey Sorokin <andreysrkn@gmail.com>
2026-06-05 01:24:27 +0300

    config file parser

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
+
+	utils "github.com/fastbear1/quack/internal/utils"
 )
 
 const (
@@ -27,28 +29,50 @@ const (
 	helpInfo = `Quack - generate migration file for goose according gorm struct models 
 information and database state. Use config file quack_config.yaml for running params.
 Uasge:
-	quack command [flags]
+  quack [flags] command
 Commands:
-	- run - quack(run and create) goose migration file
-	- help - show help information
-	- version - show current version(can be used for checking config file)
+  - run - quack(run and create) goose migration file
+  - help - show help information
+  - version - show current version(can be used for checking config file)
+  command usage example:
+    - quack help - show help information
+    - quack run - run creating a new migration files
 flags:
-	- 
-`
+  - models - directory where all gorm struct models live
+  - uri - URI connection database string
+  - dbname - database name
+  - path - directory where a new migration files will be stored
+  - exclude - exclude gorm struct models(usually it's a embeded struct or not a model struct)
+  - db-exclude - exclude database tables(for example goose_migrations table)
+  flag usage examples:
+    - quack --models=models --path=migrations --uri=postgres://user:pass@host:port/database --exclude=Base,TestUsers run
+    - quack --modesl=internal/models --path=models --uri=postgres://user:pass@host:port/database --exclude=Base --db-exclude=goose,goose_migrations run`
 )
 
 func main() {
-	//todo := flag.String("h", "", "Print todo list")
-	numbPtr := flag.Int("numb", 0, "int number")
-	flag.Parse()
-	if *numbPtr == 0 {
-		fmt.Println("Numbet not defined")
-	} else {
-		fmt.Println(*numbPtr)
+	var yconf utils.ConfigYaml
+	cnf, err := yconf.ReadConfig()
+	if err != nil {
+		fmt.Println("Can't config file")
 	}
+
+	flag.Var(&cnf.Models.Path, "models", "path to gorm models")
+	flag.Var(&cnf.Database.Uri, "uri", "database URI")
+	flag.Var(&cnf.Database.Name, "dbname", "database name")
+	flag.Var(&cnf.Migrations.Path, "path", "path tp directory with migration files")
+
+	flag.Var(&cnf.Models.Exclude, "exclude", "Exlude gorm models")
+	flag.Var(&cnf.Database.Exclude, "db-exclude", "Exlude db tables")
+
+	flag.Parse()
+	fmt.Println(cnf)
+
+	// check commands
 	var commands []string = flag.Args()
-	fmt.Println(commands)
-	fmt.Println("tail:", commands)
+
+	var conf utils.Config
+	conf.GetConfig()
+	fmt.Println(conf)
 
 	if len(commands) > 0 {
 		switch commands[0] {
--- internal/utils/config.go
+++ internal/utils/config.go
@@ -1,5 +1,15 @@
 package utils
 
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"gopkg.in/yaml.v3"
+)
+
+const configFileName string = "quack_config.yaml"
+
 type Config struct {
 	Path           string
 	Postgres_url   string
@@ -7,6 +17,109 @@ type Config struct {
 	ExcludeTables  []string
 	ExcludeModels  []string
 	MigrationsPath string
+}
+
+/*
+	Example
+project:
+  files:
+    - file1
+    - file2
+  folders:
+    - folder1
+    - folder2
+  random1:
+  random2:
+    - redundant1
+
+  version: 0.1
+  database:
+~   uri: "postgres://stexp:1!password!2@postgres:5432/stexp"
+~   name: "stexp"
+    exclude:
++     - "auth_users"
++     - "users"
++     - "goose_migrations"
+  models:
+~   path: "models"
+    exclude:
++     - "Base"
++     - "Users"
++     - "AuthUsers"
+  migrations:
+~   path: "migrations"
+
+type MyStruct struct {
+    Project struct {
+        Files   []string `yaml:"files"`
+        Folders []string `yaml:"folders"`
+    } `yaml:"project"`
+}
+
+*/
+
+// string and arrays types for yaml config
+type StringVal string
+type StringList []string
+
+func (s StringVal) String() string {
+	return string(s)
+}
+
+func (s *StringVal) Set(value string) error {
+	*s = StringVal(value)
+	return nil
+}
+
+func (s StringList) String() string {
+	return strings.Join(s, ",")
+}
+
+func (s *StringList) Set(value string) error {
+	*s = strings.Split(value, ",")
+	return nil
+}
+
+type ConfigYaml struct {
+	Version  float32 `yaml:"version"`
+	Database struct {
+		Uri     StringVal  `yaml:"uri"`
+		Name    StringVal  `yaml:"name"`
+		Exclude StringList `yaml:"exclude"`
+	} `yaml:"database"`
+	Models struct {
+		Path    StringVal  `yaml:"path"`
+		Exclude StringList `yaml:"exclude"`
+	} `yaml:"models"`
+	Migrations struct {
+		Path StringVal `yaml:"path"`
+	} `yaml:"migrations"`
+}
+
+func findConfigFile() ([]byte, error) {
+	cfile, err := os.ReadFile(fmt.Sprintf("./%s", configFileName))
+	if err != nil {
+		// try parent directory
+		cfile, err = os.ReadFile(fmt.Sprintf("../%s", configFileName))
+		fmt.Println("Found config in parent directory")
+		if err != nil {
+			return nil, err
+		}
+	}
+	fmt.Println("Found config file in current directory")
+	return cfile, nil
+}
+
+func (conf *ConfigYaml) ReadConfig() (*ConfigYaml, error) {
+	cfile, err := findConfigFile()
+	if err != nil {
+		return conf, err
+	}
+	read_err := yaml.Unmarshal(cfile, conf)
+	if read_err != nil {
+		return nil, read_err
+	}
+	return conf, err
 }
 
 func (conf *Config) GetConfig() {

c64d5ef5688bb899539173908671f5c7641dfe09
Andrey Sorokin <andreysrkn@gmail.com>
2026-06-16 21:26:05 +0300

    test for parsing input flags

--- cmd/main_test.go
+++ cmd/main_test.go
@@ -1,8 +1,22 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"strings"
 	"testing"
 )
 
 func TestParseFalgs(t *testing.T) {
+	flagsStr := "--models=models --path=migrations --uri=postgres://user:pass@host:port/database --exclude=Base,TestUsers"
+
+	flagsCmd := strings.Split(flagsStr, " ")
+
+	fmt.Println(os.Args)
+	for _, f := range flagsCmd {
+		os.Args = append(os.Args, f)
+	}
+
+	conf := ParseFlags()
+	fmt.Println(conf)
 }

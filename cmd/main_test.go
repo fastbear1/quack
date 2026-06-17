4e3560b41187e7ea7f6979970929c88f6c902c48
Andrey Sorokin <andreysrkn@gmail.com>
2026-06-17 17:55:30 +0300

    test for parsing command line args

--- cmd/main_test.go
+++ cmd/main_test.go
@@ -9,26 +9,25 @@ import (
 )
 
 func TestParseFalgs(t *testing.T) {
-	var tests = []struct {
-		name     string
+	var tt = struct {
 		flagStr  string
 		excepted []string
 	}{
-		{
-			"testing flag args parser",
-			"--models=models --path=migrations --uri=postgres://user:pass@host:port/database --exclude=Base,TestUsers",
-			[]string{"models", "migrations", "postgres://user:pass@host:port/database", "Base,TestUsers"},
-		},
+		"--models=models --path=migrations --uri=postgres://user:pass@host:port/database",
+		[]string{"models", "migrations", "postgres://user:pass@host:port/database", "Base,Test"},
 	}
+	flagsCmd := strings.Split(tt.flagStr, " ")
+	for _, f := range flagsCmd {
+		os.Args = append(os.Args, f)
+	}
+	conf := ParseFlags()
+	assert.Equal(t, conf.Models.Path.String(), tt.excepted[0])
+	assert.Equal(t, conf.Migrations.Path.String(), tt.excepted[1])
+	assert.Equal(t, conf.Database.Uri.String(), tt.excepted[2])
 
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			flagsCmd := strings.Split(tt.flagStr, " ")
-			for _, f := range flagsCmd {
-				os.Args = append(os.Args, f)
-			}
-			conf := ParseFlags()
-			assert.Equal(t, conf.Models.Path.String(), tt.excepted[0])
-		})
+	var exclude []string
+	for _, v := range conf.Models.Exclude {
+		exclude = append(exclude, v)
 	}
+	assert.Equal(t, exclude, strings.Split(tt.excepted[3], ","))
 }

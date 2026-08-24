f66ae7e8809264a0f11ca701a762225cb5817fdc
Andrey Sorokin <andreysrkn@gmail.com>
2026-08-24 17:53:51 +0300

    Integrationa testing with files comparison

--- tests/runner_test.go
+++ tests/runner_test.go
@@ -1,11 +1,13 @@
 package tests
 
 import (
+	"bufio"
 	"bytes"
 	"context"
 	"fmt"
 	"os"
 	"os/exec"
+	"strings"
 	"testing"
 	"time"
 
@@ -24,6 +26,12 @@ func TestInitDatabase(t *testing.T) {
 
 	err := proc.Run(ctx, conf, migrationFile)
 	assert.Equal(t, err, 0)
+
+	//getting migration file name
+	lsc, _ := exec.Command("ls", `./migrations/`).Output()
+	lscfile := strings.Split(string(lsc), "\n")[0]
+	// check that content of migration file is identical to original
+	assert.True(t, LinearFilesComparison("../sandbox/case1/migrations/init-database.sql", fmt.Sprintf("./migrations/%s", lscfile)))
 
 	home := os.Getenv("HOME")
 	cmd := exec.Command(fmt.Sprintf("%s/go/bin/goose", home), "postgres", "user=quack dbname=quack password=pass host=postgres sslmode=disable", "-dir=migrations", "up")
@@ -50,6 +58,12 @@ func TestManageTableColumns(t *testing.T) {
 	err := proc.Run(ctx, conf, migrationFile)
 	assert.Equal(t, err, 0)
 
+	//getting migration file name
+	lsc, _ := exec.Command("ls", `./migrations/`).Output()
+	lscfile := strings.Split(string(lsc), "\n")[1]
+	// check that content of migration file is identical to original
+	assert.True(t, LinearFilesComparison("../sandbox/case2/migrations/manage-columns.sql", fmt.Sprintf("./migrations/%s", lscfile)))
+
 	home := os.Getenv("HOME")
 	cmd := exec.Command(fmt.Sprintf("%s/go/bin/goose", home), "postgres", "user=quack dbname=quack password=pass host=postgres sslmode=disable", "-dir=migrations", "up")
 
@@ -74,6 +88,12 @@ func TestAlterTableColumns(t *testing.T) {
 
 	err := proc.Run(ctx, conf, migrationFile)
 	assert.Equal(t, err, 0)
+
+	//getting migration file name
+	lsc, _ := exec.Command("ls", `./migrations/`).Output()
+	lscfile := strings.Split(string(lsc), "\n")[2]
+	// check that content of migration file is identical to original
+	assert.True(t, LinearFilesComparison("../sandbox/case3/migrations/alter-columns.sql", fmt.Sprintf("./migrations/%s", lscfile)))
 
 	home := os.Getenv("HOME")
 	cmd := exec.Command(fmt.Sprintf("%s/go/bin/goose", home), "postgres", "user=quack dbname=quack password=pass host=postgres sslmode=disable", "-dir=migrations", "up")
@@ -100,6 +120,12 @@ func TestTableIndices(t *testing.T) {
 	err := proc.Run(ctx, conf, migrationFile)
 	assert.Equal(t, err, 0)
 
+	//getting migration file name
+	lsc, _ := exec.Command("ls", `./migrations/`).Output()
+	lscfile := strings.Split(string(lsc), "\n")[3]
+	// check that content of migration file is identical to original
+	assert.True(t, LinearFilesComparison("../sandbox/case4/migrations/table-indices.sql", fmt.Sprintf("./migrations/%s", lscfile)))
+
 	home := os.Getenv("HOME")
 	cmd := exec.Command(fmt.Sprintf("%s/go/bin/goose", home), "postgres", "user=quack dbname=quack password=pass host=postgres sslmode=disable", "-dir=migrations", "up")
 
@@ -125,6 +151,12 @@ func TestTableConstraints(t *testing.T) {
 	err := proc.Run(ctx, conf, migrationFile)
 	assert.Equal(t, err, 0)
 
+	//getting migration file name
+	lsc, _ := exec.Command("ls", `./migrations/`).Output()
+	lscfile := strings.Split(string(lsc), "\n")[4]
+	// check that content of migration file is identical to original
+	assert.True(t, LinearFilesComparison("../sandbox/case5/migrations/table-constraints.sql", fmt.Sprintf("./migrations/%s", lscfile)))
+
 	home := os.Getenv("HOME")
 	cmd := exec.Command(fmt.Sprintf("%s/go/bin/goose", home), "postgres", "user=quack dbname=quack password=pass host=postgres sslmode=disable", "-dir=migrations", "up")
 
@@ -149,4 +181,48 @@ func getTestConfig() *utils.ConfigYaml {
 	conf.Verbose = false
 
 	return &conf
+}
+
+func LinearFilesComparison(leftFile string, rightFile string) bool {
+	f1, err := os.Open(leftFile)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer f1.Close()
+
+	f2, err := os.Open(rightFile)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer f2.Close()
+
+	bf1 := make([][]byte, 0)
+	bf2 := make([][]byte, 0)
+
+	scanner := bufio.NewScanner(f1)
+	for scanner.Scan() {
+		bf1 = append(bf1, scanner.Bytes())
+	}
+
+	scanner = bufio.NewScanner(f2)
+	for scanner.Scan() {
+		bf2 = append(bf2, scanner.Bytes())
+	}
+
+	var cutline int
+	bff := bf2[:len(bf2)]
+	for _, line := range bf1 {
+		for i, ln := range bff {
+			if bytes.Equal(line, ln) {
+				cutline = i
+				break
+			}
+		}
+		bff = append(bff[0:cutline], bff[cutline+1:]...)
+	}
+
+	if len(bff) == 0 {
+		return true
+	}
+	return false
 }
